d547c4d9c834432f811214f3f01dd89c0f025ae4
Luis PM <luisp237@gmail.com>
2020-06-27 06:25:25 -0400

    pivot to redux for state management over useState()

--- github-user/src/index.js
+++ github-user/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { githubUserReducer } from './store/reducer';
 
+const store = createStore(githubUserReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ github-user/src/store/reducer/index.js
@@ -0,0 +1,16 @@
+
+
+
+const initialState = {
+    users: [],
+    currentPage: 'https://api.github.com/users?since=0',
+    isFetchingAll: false,
+    isFetchingInd: false,
+    userInfo: null,
+    repos: null,
+}
+
+
+export githubUserReducer = (state=initialState, ) => {
+
+}
